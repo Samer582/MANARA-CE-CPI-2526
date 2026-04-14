56876a618e5dd85932ad5c222cd71ad1f1c4dbd9
Samer Sulaiman <samer56sliman3@gmail.com>
2026-04-14 20:59:29 +0300

    Added a new Example

---
+++ Task08.cpp
@@ -0,0 +1,25 @@
+/*              Manara University
+                2025 - 2026
+                Computer Programming I
+                Civil Engineering
+
+This program is used to find the second largest number in an array
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 7};
+    int n = 5;
+
+    int largest = arr[0];
+    int second = -1;
+
+    // TODO: find largest
+    
+
+    // TODO: find second largest
+    
+
+    cout << second;
+}
